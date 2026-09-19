cd229faa143c6cfed8bd6f9667079125556cd67c
Clara <laalaaning@gmail.com>
2026-09-19 23:39:18 +0800

    feat: 調整 RedButton 功能

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,7 @@
 import "./App.css";
-import RedButton from "./components/RedButton";
-import { ChevronRight } from "lucide-react";
 
 function App() {
-  return (
-    <RedButton
-      // color="danger"
-      redButton={[
-        <div className="flex items-center gap-1">
-          <span className="font-bold">NT$ 1,420</span>
-          <ChevronRight className="w-5 h-5" />
-        </div>,
-        <div className="flex items-center gap-1">
-          <span className="font-bold">NT$ 1,420</span>
-          <ChevronRight className="w-5 h-5" />
-        </div>,
-      ]}
-    />
-  );
+  return <></>;
 }
 
 export default App;
