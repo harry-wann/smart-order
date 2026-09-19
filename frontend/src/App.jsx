c62a483238d4d45b25720c83f0e921b856168298
Clara <laalaaning@gmail.com>
2026-09-19 23:35:19 +0800

    feat: 新增 RedButton 深淺色樣式

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,23 @@
 import "./App.css";
+import RedButton from "./components/RedButton";
+import { ChevronRight } from "lucide-react";
 
 function App() {
-  return <></>;
+  return (
+    <RedButton
+      // color="danger"
+      redButton={[
+        <div className="flex items-center gap-1">
+          <span className="font-bold">NT$ 1,420</span>
+          <ChevronRight className="w-5 h-5" />
+        </div>,
+        <div className="flex items-center gap-1">
+          <span className="font-bold">NT$ 1,420</span>
+          <ChevronRight className="w-5 h-5" />
+        </div>,
+      ]}
+    />
+  );
 }
 
 export default App;
--- frontend/src/components/RedButton.jsx
+++ frontend/src/components/RedButton.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 // 加圖 父元件要 import { icon } from "lucide-react";
-// 用法: <RedButton redButton={[tag1, tag2]} />
+// 用法: <RedButton color="danger" redButton={[tag1, tag2]} />
+// 是否為 danger 決定要不要使用 color
 // ex. <RedButton redButton={[
 //     <div className="flex items-center gap-1">
 //       <span className="font-bold">NT$ 1,420</span>
@@ -8,14 +9,17 @@ import React from "react";
 //     </div>,
 //   ]}
 //  />
-function RedButton({ redButton, onClick }) {
+function RedButton({ redButton, color = "default", ...props }) {
   const childCount = React.Children.count(redButton);
   const justifyClass = childCount > 1 ? "justify-between" : "justify-center";
-
+  const themeClass =
+    color == "danger"
+      ? "bg-dark-red-btn hover:bg-[color-mix(in_srgb,var(--color-dark-red-btn),black_8%)]"
+      : "bg-red-btn hover:bg-[color-mix(in_srgb,var(--color-red-btn),black_8%)]";
   return (
     <button
-      onClick={onClick}
-      className={`flex w-full items-center px-4 py-2 text-white rounded h-[36px] md:h-[44px] lg:h-[52px] bg-red-btn transition-all duration-300 hover:bg-[color-mix(in_srgb,var(--color-red-btn),black_8%)] active:scale-[0.98] disabled:opacity-40 disabled:cursor-not-allowed ${justifyClass}`}
+      {...props}
+      className={`flex w-full items-center px-4 py-2 text-white rounded h-[36px] md:h-[44px] lg:h-[52px] ${themeClass} transition-all duration-300 active:scale-[0.98] disabled:opacity-40 disabled:cursor-not-allowed ${justifyClass}`}
     >
       {redButton.map((item, index) => {
         return (
