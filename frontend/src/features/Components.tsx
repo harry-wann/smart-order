d3320d42381e6b7af046891edd02f8aab39f7249
Harry <cool890104@gmail.com>
2026-09-21 01:30:09 +0800

    feat: 點餐菜單項目 row item

--- frontend/src/features/Components.tsx
+++ frontend/src/features/Components.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import MenuItemRow from '@/components/order/MenuItemRow'
 type Props = {}
 
 export default function Components({}: Props) {
@@ -28,7 +29,43 @@ export default function Components({}: Props) {
 
       <article className="w-full h-full flex flex-col">
         <h1>DS-06｜品項列與通知</h1>
-        
+        <div className="flex gap-3">
+          <div className="flex flex-col flex-1 p-2 gap-3">
+            <MenuItemRow
+              img="123"
+              title="安格斯霜降牛五花"
+              desc="厚切 3mm，涮 8 秒最好吃。油花分布均勻，是我們賣最好的一款。"
+              price={1234}
+              isSoldOut={false}
+              isPopular={true}
+              onClick={ () => console.log("TEST") }
+            />
+
+            <MenuItemRow
+              img="123"
+              title="安格斯霜降牛五花"
+              desc="厚切 3mm，涮 8 秒最好吃。油花分布均勻，是我們賣最好的一款。"
+              price={1234}
+              isSoldOut={false}
+              isPopular={false}
+              onClick={ () => console.log("TEST") }
+            />
+
+            <MenuItemRow
+              img="123"
+              title="安格斯霜降牛五花"
+              desc="厚切 3mm，涮 8 秒最好吃。油花分布均勻，是我們賣最好的一款。"
+              price={1234}
+              isSoldOut={true}
+              isPopular={false}
+              onClick={ () => console.log("TEST") }
+            />
+          </div>
+
+          <div className="flex flex-col flex-1 p-2">
+
+          </div>
+        </div>
       </article>
 
     </div>
