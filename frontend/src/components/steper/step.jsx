d5338f0fd13ea112b7f8847d7ee25e3e0f74ad32
YiTing505 <tina2030318@gmail.com>
2026-09-22 22:05:26 +0800

    步進器

--- frontend/src/components/steper/step.jsx
+++ frontend/src/components/steper/step.jsx
@@ -1,21 +1,18 @@
-import Counter from "./counter.jsx";
-
+import Counter from './counter.jsx'
 
 function Step() {
   return (
     <div className="flex flex-col items-start gap-10 p-10">
-
       {/* 小型：102 × 30 */}
       <Counter size="small" initialCount={10} />
 
       {/* 大型：186 × 58 */}
       <Counter size="large" initialCount={1} />
 
-      {/* 尚未選擇：28 × 28 */}
+      {/* 未選擇：28 × 28 */}
       <Counter size="small" initialCount={0} />
-
     </div>
-  );
+  )
 }
 
-export default Step;
+export default Step
