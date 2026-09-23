90b7aa96bd41fd45d8849081e0b2596aea3df190
YiTing505 <tina2030318@gmail.com>
2026-09-23 16:51:46 +0800

    調整

--- frontend/src/features/components-demo/DS03.jsx
+++ frontend/src/features/components-demo/DS03.jsx
@@ -7,11 +7,13 @@ import Step from '../../components/steper/step'
 
 export default function DS03() {
   return (
-    <div>
-      <FormInput />
-      <Verify />
-      <Open />
-      <Step />
-    </div>
+    <DemoPage code="DS-07" title="其他元件" description="放置尚未分類的共用元件與互動模式。">
+      <div>
+        <FormInput />
+        <Verify />
+        <Open />
+        <Step />
+      </div>
+    </DemoPage>
   )
 }
